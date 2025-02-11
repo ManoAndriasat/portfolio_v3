5da9d4470151e0e023cbdc68060f69f59b114da2
Mano ANDRIASATARINTSOA <159690397+ManoAndriasat@users.noreply.github.com>
2025-02-11 19:44:57 +0300

    Update Marquee.jsx

--- src/app/components/Marquee.jsx
+++ src/app/components/Marquee.jsx
@@ -9,11 +9,11 @@ export default function ModifMarquee() {
             <Marquee gradient={false} speed={50}>
                 {[...Array(10)].map((_, index) => (
                     <React.Fragment key={index}>
-                    <p className='marquee text-black inline-flex items-center'>
+                    <p className='marquee text-[#1c1b19] inline-flex items-center'>
                         creative{' '}
-                        <span className="bg-black text-white p-3 ml-2 rounded-lg">developer</span>{' '}
+                        <span className="bg-[#1c1b19] text-white p-3 ml-2 rounded-lg">developer</span>{' '}
                         <span className="mx-10">
-                        <BsFillSuitSpadeFill className="text-3xl text-black" />
+                        <BsFillSuitSpadeFill className="text-3xl text-[#1c1b19]" />
                         </span>
                     </p>
                     </React.Fragment>
