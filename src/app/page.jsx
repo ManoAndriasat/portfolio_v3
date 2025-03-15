9c8489ba19706362016a84a3244b2a139deabc24
Mano ANDRIASATARINTSOA <159690397+ManoAndriasat@users.noreply.github.com>
2025-03-15 10:27:02 +0300

    style modification

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -299,7 +299,7 @@ export default function Home() {
               className="skill h-content lg:h-[60vh] p-5 lg:px-10"
               style={{ backgroundColor: skill.bgColor }}
             >
-              <p className='text-[30px] lg:text-[6em] text-[#1c1b19] font-extrabold uppercase pb-10'>
+              <p className='skill-title text-[30px] lg:text-[6em] text-[#1c1b19] font-extrabold uppercase pb-7'>
                 {skill.title}
               </p>
               <p className="skill-description leading-none text-[25px] lg:text-[40px]">
